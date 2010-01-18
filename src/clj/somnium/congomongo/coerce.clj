6bf22de379996dfeac85d5ff5f3093e331acb783
Steve Purcell <steve@sanityinc.com>
2010-01-18 16:47:08 +0000

    Add initial support for GridFS with new fns insert-file!, fetch-files, destroy-file! etc.
    Implementation notes:
    
    This is the bare minimum code to get data in and out of GridFS.
    
    The collections that back GridFS internally use an ObjectClass of
    GridFSDBFile, so the regular ClojureDBObject hack can't be used to get
    painless coercion. To reuse the fast conversion code in
    ClojureDBObject for this purpose, I've made public the static method
    ClojureDBObject/toClojureMap. The :gridfs hint for 'coerce' seemed less
    messy than leaking another function into congomongo.clj.
    
    Some reflection will probably be used in insert-file! and
    write-file-to, but this seems preferable than repeating the list of
    possible argument types for the wrapped overloaded methods.
    
    There's no atomic update support in GridFS, but an update-file! fn can
    be written, together with wrappers for other GridFS functionality,
    such as listing all files in a bucket.
    
    The Java GridFS implementation curiously leaves the uploadDate
    attribute of files blank by default, in addition to the contentType
    field. It may be desirable to fill those fields in congomongo, but I
    haven't done that at this stage.

--- src/clj/somnium/congomongo/coerce.clj
+++ src/clj/somnium/congomongo/coerce.clj
@@ -5,13 +5,35 @@
         [clojure.contrib.core :only [seqable?]])
   (:import [somnium.congomongo ClojureDBObject]
            [clojure.lang IPersistentMap]
+           [com.mongodb DBObject]
+           [com.mongodb.gridfs GridFSFile]
            [com.mongodb.util JSON]))
 
 (defvar *keywordize* true
   "Set this to false to prevent ClojureDBObject from setting string keys to keywords")
 
-(defunk 
-  coerce
+
+(defn- dbobject->clojure
+  "Not every DBObject returned from Mongo is a ClojureDBObject,
+   since we can't setObjectClass on the collections used to back GridFS;
+   those collections have GridFSDBFile as their object class.
+
+   This function uses ClojureDBObject to marshal such DBObjects
+   into Clojure structures; in practice, this applies only to GridFSFile
+   and its subclasses."
+  [#^DBObject f keywordize]
+  (ClojureDBObject/toClojureMap
+   (try (.toMap f)
+        ;; DBObject provides .toMap, but the implementation in
+        ;; subclass GridFSFile unhelpfully throws
+        ;; UnsupportedOperationException
+        (catch UnsupportedOperationException e
+          (let [keys (.keySet f)]
+            (zipmap keys (map #(.get f %) keys)))))
+   keywordize))
+
+
+(defunk coerce
   {:arglists '([obj [:from :to] {:many false}])
    :doc
    "takes an object, a vector of keywords:
@@ -28,6 +50,7 @@
               [:mongo   :clojure] #(.toClojure #^ClojureDBObject %
                                                #^Boolean/TYPE *keywordize*)
               [:mongo   :json   ] #(.toString #^ClojureDBObject %)
+              [:gridfs  :clojure] #(dbobject->map #^GridFSFile %)
               [:json    :clojure] #(binding [*json-keyword-keys* *keywordize*] (read-json %))
               [:json    :mongo  ] #(JSON/parse %)
               :else               (throw (RuntimeException.
@@ -40,3 +63,4 @@
   "only used for creating argument object for :only"
   [fields]
   (ClojureDBObject. #^IPersistentMap (zipmap fields (repeat 1))))
+
