5f7440f1bf6ba09c9ec5a8e582a5f029a3940604
Steve Purcell <steve@sanityinc.com>
2010-01-21 21:42:15 +0000

    WIP: Integrate the lein-javac plugin (http://github.com/antoniogarrote/lein-javac)

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject org.clojars.somnium/congomongo
   "0.1.1-SNAPSHOT"
   :description "clojure-friendly api for MongoDB"
+  :java-source-path "src/java"
+  :javac-fork "true"
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                 [org.clojars.somnium/mongo-java-driver "1.1.0-SNAPSHOT"]
-                 [org.clojars.somnium/clojure-db-object "0.1.1-SNAPSHOT"]]
+                 [org.clojars.somnium/mongo-java-driver "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
-                     [org.clojars.somnium/user "0.1.0-SNAPSHOT"]]
+                     [org.clojars.somnium/user "0.1.0-SNAPSHOT"]
+                     [lein-javac "0.0.1-SNAPSHOT"]]
   :source-path "src/clj")
